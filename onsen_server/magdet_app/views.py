f0555a1f3319ed8aaebc693e562d6431a12f8b42
segfau-yama <s1701120@sendai-nct.jp>
2022-06-26 18:23:49 +0900

    v0.0.1(α版) webページ変更

--- onsen_server/magdet_app/views.py
+++ onsen_server/magdet_app/views.py
@@ -15,9 +15,13 @@ class lock_confirm(ListView):
 @csrf_exempt
 def esp_input(request):
     datas = json.loads(request.body)
-    ava = availability.objects.get(pk=4)
-    update_lock = not bool(datas['lock'])
+    ava = availability.objects.get(pk=int(datas['room']))
+    bool(datas['lock'])
+    ava.lock = bool(datas['lock'])
+    ava.save()
+    """
     if ava.lock != update_lock:
         ava.lock = update_lock
         ava.save()
+    """
     return HttpResponse("str(datas)")
