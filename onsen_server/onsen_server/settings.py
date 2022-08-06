b3e3090569af613fd030dab9ecffc753e03d1350
hayasaka11 <s1701100@sendai-nct.jp>
2022-08-06 17:26:03 +0900

    データベースの画像表示対応

--- onsen_server/onsen_server/settings.py
+++ onsen_server/onsen_server/settings.py
@@ -113,6 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA＿ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
